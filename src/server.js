9cb68b714ea7b10bfed7ca4cfb5ac453eaea102d
Carlos A Hernández C <xcarlosdesarrolladorx@gmail.com>
2021-10-04 11:17:28 -0500

    Delete frontend

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, '../public')));
+// app.use(express.static(path.join(__dirname, '../public')));
 
 const route = require("./network");
 route(app);
